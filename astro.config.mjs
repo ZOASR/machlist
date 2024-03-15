91f2538215a16a9136bf28e591df722cd2e33de8
ZOASR <44146518+ZOASR@users.noreply.github.com>
2024-03-16 00:50:22 +0200

    build: updated astro config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,17 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-	output: "server",
-	integrations: [
-		tailwind({
-			nesting: true,
-		}),
-		svelte(),
-	],
-	adapter: vercel({
-		imageService: true,
-	}),
+  output: "server",
+  integrations: [tailwind({
+    nesting: true
+  }), svelte(), icon()],
+  adapter: vercel({
+    imageService: true
+  })
 });
