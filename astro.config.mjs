8e7ff79bdfb8310c7681d4c149e23391e79e628e
ZOASR <44146518+ZOASR@users.noreply.github.com>
2024-03-16 19:45:03 +0200

    config: added mdx integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,27 +2,24 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import svelte from '@astrojs/svelte';
 import vercel from '@astrojs/vercel/serverless';
-
 import icon from 'astro-icon';
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-	output: 'server',
-	integrations: [
-		tailwind({
-			nesting: true
-		}),
-		svelte(),
-		icon()
-	],
-	adapter: vercel({
-		imageService: true
-	}),
-	i18n: {
-		defaultLocale: 'ar',
-		locales: ['en', 'ar'],
-		routing: {
-			prefixDefaultLocale: true
-		}
-	}
+  output: 'server',
+  integrations: [tailwind({
+    nesting: true
+  }), svelte(), icon(), mdx()],
+  adapter: vercel({
+    imageService: true
+  }),
+  i18n: {
+    defaultLocale: 'ar',
+    locales: ['en', 'ar'],
+    routing: {
+      prefixDefaultLocale: true
+    }
+  }
 });
