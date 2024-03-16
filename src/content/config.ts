9a682273b0c918290b2cff9c9976f6772b037df3
ZOASR <44146518+ZOASR@users.noreply.github.com>
2024-03-16 19:45:59 +0200

    content: content config file

---
+++ src/content/config.ts
@@ -0,0 +1,36 @@
+// 1. Import utilities from `astro:content`
+import { z, defineCollection } from 'astro:content';
+
+// 2. Define a `type` and `schema` for each collection
+const productsCollection = defineCollection({
+	type: 'content',
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			thumbnail: image(),
+			order: z.number(),
+			production_rate: z.number().optional(),
+			model: z.string().optional(),
+			nested: z.boolean().optional(),
+			isLine: z.boolean().optional()
+		})
+});
+const designsCollection = defineCollection({
+	type: 'content',
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			thumbnail: image(),
+			order: z.number(),
+			production_rate: z.number().optional(),
+			model: z.string().optional(),
+			nested: z.boolean().optional(),
+			isLine: z.boolean().optional()
+		})
+});
+
+// 3. Export a single `collections` object to register your collection(s)
+export const collections = {
+	productss: productsCollection,
+	designs: designsCollection
+};
