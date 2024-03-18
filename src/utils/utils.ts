374a7de934f5e5b0f3b08ceeca4f1b9ec82c43b6
ZOASR <44146518+ZOASR@users.noreply.github.com>
2024-03-18 23:41:29 +0200

    refactor: removed tabs definition from utils file

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,23 +1,13 @@
+export function getLocaledName(name: string, locale: string) {
+	return name
+		.replace("catalog", locale === "en" ? "Catalog" : "كاتالوج")
+		.replace("design", locale === "en" ? "Design" : "التصميم")
+		.replace("drawings", locale === "en" ? "Drawings" : "الرسومات")
+		.replace("gallery", locale === "en" ? "Gallery" : "الصور");
+}
+
 export type Tab = {
 	id: number;
 	name: { en: string; ar: string };
-	link: string;
+	slug: string;
 };
-
-export const augerTabs: Tab[] = [
-	{
-		id: 1,
-		name: { en: 'Catalog', ar: 'كاتالوج' },
-		link: '/auger_conveyor/catalog'
-	},
-	{
-		id: 2,
-		name: { en: 'Design', ar: 'التصميم' },
-		link: '/auger_conveyor/design'
-	},
-	{
-		id: 3,
-		name: { en: 'Gallery', ar: 'الصور' },
-		link: '/auger_conveyor/gallery'
-	}
-];
