6ab07f9a885e51abc29517d7a4473cc2938171d7
ZOASR <44146518+ZOASR@users.noreply.github.com>
2024-03-25 22:06:54 +0200

    style: style changes

--- src/components/svelte/LanguageSelector.svelte
+++ src/components/svelte/LanguageSelector.svelte
@@ -68,7 +68,7 @@
 		<slot />
 	</Select.Trigger>
 	<Select.Content
-		class="rounded-xl border-2 border-skin-primary p-2 space-y-4 text-white shadow-xl outline-none bg-skin-neutral"
+		class="rounded-xl p-2 space-y-4 text-white shadow-xl outline-none bg-skin-neutral"
 		transition={fadeAndScale}
 		transitionConfig={{ duration: 100, easing: quintInOut }}
 		style="transform-origin: top center;"
