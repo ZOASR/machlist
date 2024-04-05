f5f3bc924fd44d597e47b50eada21e3bc64ada51
ZOASR <44146518+ZOASR@users.noreply.github.com>
2024-04-06 00:17:33 +0200

    refactor: refactored localization

---
+++ src/i18n/ui.ts
@@ -0,0 +1,133 @@
+export const languages = {
+	en: "English",
+	ar: "العربية"
+};
+
+export const defaultLang = "ar";
+
+export const ui = {
+	en: {
+		"meta.title": "Machlist",
+		"meta.description":
+			"Machlist is an Egyptian company established at 2005 in Cairo for machines and production lines.",
+		// Header
+		"header.title": "Machlist",
+		"nav.home": "Home",
+		"nav.about": "About",
+		"nav.products": "Products",
+		"nav.contact": "Contact Us",
+		// Hero
+		"hero.slogan": "We Design,",
+		"hero.subtitle": "You Profit",
+		"hero.description":
+			"From design ideas to working production lines. we power your industrial lines and machines",
+		"hero.buttons.seeProducts": "See our Products",
+		"hero.buttons.contactUs": "Contact Us",
+		// Stats
+		"stats.title": "What we offer",
+		"stats.description":
+			"We offer over 50 machine designs crafted with over 20 years of experience",
+		// Steps
+		"steps.title": "How we do it?",
+		// GetStarted
+		"getStarted.title": "Where to start?",
+		"getStarted.first.title": "1. Browse our products",
+		"getStarted.first.description":
+			"Browse over 50 machine designs and production lines and select the perfect one for your needs",
+		"getStarted.second.title": "2. Contact us",
+		"getStarted.second.description": "Contact our sales team",
+		"getStarted.third.title": "3. Get started",
+		"getStarted.third.description":
+			"Start integrating your perfect production line with us",
+		// Feedback
+		"feedback.title": "Already a customer?",
+		"feedback.description": "We'd love to hear from you",
+		name: "Name",
+		email: "Email",
+		phone: "Phone",
+		message: "Your feedback",
+		submit: "Send",
+		// Footer
+		"footer.nav": "Website",
+		"footer.social": "Social Media",
+		"footer.location": "Our Location",
+		"footer.location.address": "18 Omar Ibn El-khattab St. - Faisal - Giza",
+		"footer.contact": "Contact Us",
+		// Product
+		"product.title": "Product",
+		"product.model": "Model",
+		"product.productionRate": "Production rate",
+		"product.productionUnit": "Kg/h",
+		"product.machine": "Machine",
+		"product.line": "Production line",
+		// Filter
+		"filter.all": "All",
+		"filter.machine": "Machines only",
+		"filter.line": "Production lines only",
+		"filter.search": "Search",
+		"filter.emptyModel": "Select a model...",
+		"filter.noProducts": "No products found for this search"
+	},
+	ar: {
+		"meta.title": "ماك-ليست",
+		"meta.description":
+			'تأسست شركة " ماك-ليســـت " عام 2005 بالقاهرة وتعمل في مجال ماكينات وخطوط الإنتاج المختلفة',
+		// Header
+		"header.title": "ماك-ليست",
+		"nav.home": "الصفحة الرئيسية",
+		"nav.about": "عنّا",
+		"nav.products": "متجاتنا",
+		"nav.contact": "اتصل بنا",
+		// Hero
+		"hero.slogan": "نحن نصمم,",
+		"hero.subtitle": "أنت تربح",
+		"hero.description":
+			"من تصميمات إلى خطوط إنتاجية . نحن ندعم كافة أنواع	الحلول الصناعية",
+		"hero.buttons.seeProducts": "تصفح منتجاتنا",
+		"hero.buttons.contactUs": "اتصل بنا",
+		// Stats
+		"stats.title": "ما نقدمه",
+		"stats.description":
+			"نحن نقدم ما يعادي الـ50 تصميم للماكينات و خطوط الإنتاج تم تصميمها و بعناية بمساعدة خبراتنا التي تفوق ال 20 عاماً في مجال التصنيع",
+		// Steps
+		"steps.title": "كيف تصلك خدماتنا",
+		// GetStarted
+		"getStarted.title": "من أين أبدأ؟",
+		"getStarted.first.title": "1.  تصفح منتجاتنا",
+		"getStarted.first.description":
+			"تصفح من بين أكثر من 50 تصميم و خط إنتاج و اختر المنتج المناسب لك",
+		"getStarted.second.title": "2. تواصل معنا",
+		"getStarted.second.description": "تواصل مع فريق المبيعات",
+		"getStarted.third.title": "3. ابدأ",
+		"getStarted.third.description": "ابدأ بتشغيل خط الانتاج الخاص بك",
+		// Feedback
+		"feedback.title": "هل أنت عميل؟",
+		"feedback.description": "نريد أن نسمع آرائك",
+		name: "الاسم",
+		email: "عنوان البريد الإلكتروني:",
+		phone: "رقم الهاتف:",
+		message: "رأيك",
+		submit: "ارسال",
+		// Footer
+		"footer.nav": "تصفح الموقع",
+		"footer.social": "صفحات التواصل الاجتماعي",
+		"footer.location": "موقعنا",
+		"footer.location.address": "١٨ ش عمر ابن الخطاب - فيصل - الجيزة",
+		"footer.contact": "اتصل بنا",
+		// Product
+		"product.title": "منتج",
+		"product.model": "موديل",
+		"product.productionRate": "معدل الانتاج",
+		"product.productionUnit": "كج/ساعة",
+		"product.machine": "ماكينة",
+		"product.line": "خط انتاج",
+		// Filter
+
+		"filter.all": "الكل",
+		"filter.machine": "الماكينات فقط",
+		"filter.line": "خطوط الإنتاج فقط",
+		"filter.search": "البحث",
+		"filter.emptyModel": "اختر موديل...",
+		"filter.noProducts": "لا توجد منتجات لهذا البحث"
+	}
+} as const;
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -10,4 +10,5 @@ export type Tab = {
 	id: number;
 	name: { en: string; ar: string };
 	slug: string;
+	tabSlug: string;
 };
