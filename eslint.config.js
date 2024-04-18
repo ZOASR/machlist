7cb8896ebbea28f6bc13a7524c06767c126beeed
ZOASR <44146518+ZOASR@users.noreply.github.com>
2024-04-18 16:45:58 +0200

    chore: added astro eslint plugin

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,20 @@
 import js from "@eslint/js";
-
 import globals from "globals";
+import eslintPluginAstro from "eslint-plugin-astro";
 
 export default [
 	js.configs.recommended,
 	{
 		ignores: [
-			"**/node_modules/**",
-			"**/dist/**",
-			"**/build/**",
-			"**/.vercel/**/*"
+			"node_modules/**",
+			"dist/**",
+			"build/**",
+			".vercel/**/*",
+			".astro/**/*",
+			"src/env.d.ts"
 		]
 	},
+	...eslintPluginAstro.configs["flat/recommended"],
 	{
 		languageOptions: {
 			globals: {
